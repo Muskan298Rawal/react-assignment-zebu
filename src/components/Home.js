7390a18320563c3d1e241a53c4f78b15747fecd9
Muskan Rawal <rawalmuskan298@gmail.com>
2022-03-08 00:10:35 +0530

    added save functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,63 +1,81 @@
+
 import { Button, TextField } from "@mui/material";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+ 
 function Home() {
-  const [data, setData] = useState({
-    name: "",
-    description: "",
-    images: [],
-  });
-
-  const handleChange = (e) => {
-    setData({ ...data, [e.target.id]: e.target.value });
-  };
-
-  const handleImages = (e) => {
-    console.log(URL.createObjectURL(e.target.files[0]));
-  };
-
-  return (
-    <div>
-      <h3>Create project</h3>
-      <div className="outer-box">
-        <div>
-          <input
-            className="inner-box"
-            multiple
-            type="file"
-            onChange={handleImages}
-          />
-        </div>
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <TextField
-            id="name"
-            label="Name"
-            type="text"
-            style={{ margin: "10px 15px" }}
-            onChange={handleChange}
-          />
-          <TextField
-            id="description"
-            label="Description"
-            type="text"
-            style={{ margin: "10px 15px" }}
-            onChange={handleChange}
-          />
-          <Link
-            to={{
-              pathname: "/label-images",
-              state: {
-                name: "muskan",
-              },
-            }}
-          >
-            <button className="btn">Create</button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+ const [data, setData] = useState({
+   name: "",
+   description: "",
+   images: [],
+ });
+ 
+ let navigate = useNavigate();
+ 
+ useEffect(() => {
+   localStorage.setItem("ImgData", JSON.stringify(data));
+ }, [data]);
+ 
+ const handleChange = (e) => {
+   setData({ ...data, [e.target.id]: e.target.value });
+ };
+ 
+ const handleImages = (e) => {
+   const tempArr = [];
+ 
+   [...e.target.files].forEach((file) => {
+     tempArr.push({
+       data: file,
+       url: URL.createObjectURL(file),
+     });
+   });
+   setData({ ...data, images: tempArr });
+ };
+ 
+ return (
+   <div>
+     <h3>Create project</h3>
+     <div className="outer-box">
+       <div>
+         <input
+           className="inner-box"
+           multiple
+           type="file"
+           onChange={handleImages}
+         />
+       </div>
+       <div style={{ display: "flex", flexDirection: "column" }}>
+         <TextField
+           id="name"
+           label="Name"
+           type="text"
+           style={{ margin: "10px 15px" }}
+           onChange={handleChange}
+         />
+         <TextField
+           id="description"
+           label="Description"
+           type="text"
+           style={{ margin: "10px 15px" }}
+           onChange={handleChange}
+         />
+ 
+         <Button
+           className="btn"
+           disabled={
+             data.images.length == 0 ||
+             data.name.length == 0 ||
+             data.description.length == 0
+           }
+           onClick={() => navigate("/label-images")}
+         >
+           Create
+         </Button>
+         {/* </Link> */}
+       </div>
+     </div>
+   </div>
+ );
 }
-
+ 
 export default Home;
--- src/components/Label.js
+++ src/components/Label.js
@@ -1,16 +1,90 @@
-import React from 'react'
-import { useLocation, useParams } from 'react-router-dom';
-
+import { Button, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+ 
 function Label() {
-    const  params  = useParams();
-    const location = useLocation()
-    // const { from } = location.state
-    console.log("test",location, params)
-    return (
-        <div>
-            label
-        </div>
-    )
+ const [data, setData] = useState(JSON.parse(localStorage.getItem("ImgData")));
+ const [label, setLabel] = useState("");
+ const [index, setIndex] = useState(0);
+ 
+ const handleChange = (e) => {
+   setLabel(e.target.value);
+ };
+ 
+ const handlePrev = () => {
+   setIndex((index) => index - 1);
+ };
+ 
+ const handleNext = () => {
+   setIndex((index) => index + 1);
+ };
+ 
+ const downloadFile = ({ data, fileName, fileType }) => {
+   const blob = new Blob([data], { type: fileType });
+   const a = document.createElement("a");
+   a.download = fileName;
+   a.href = window.URL.createObjectURL(blob);
+   const clickEvt = new MouseEvent("click", {
+     view: window,
+     bubbles: true,
+     cancelable: true,
+   });
+   a.dispatchEvent(clickEvt);
+   a.remove();
+ };
+ 
+ const handleSave = (e) => {
+   e.preventDefault();
+   let objData = {
+     label: label,
+     url: data.images,
+     name: data.name,
+     description: data.description,
+   };
+ 
+   downloadFile({
+     data: JSON.stringify(objData),
+     fileName: "users.json",
+     fileType: "text/json",
+   });
+   setLabel("");
+ };
+ 
+ return (
+   <div className="label-data">
+   <div className="outer-div">
+     <TextField
+       id="label"
+       label="label"
+       type="text"
+       style={{ margin: "10px 15px" }}
+       onChange={handleChange}
+     />
+ 
+     <img src={data.images[index].url} height="200px" width="200px" />
+     </div>
+     <div className="label-btn">
+       <Button onClick={handlePrev} disabled={index == 0} className="btn" style={{ marginRight: "10px " }}>
+         Prev
+       </Button>
+       <Button
+         onClick={handleNext}
+         disabled={index == data.images.length - 1}
+         className="btn"
+         style={{ marginRight: "10px " }}
+       >
+         Next
+       </Button>
+       <Button
+         onClick={handleSave}
+         className="btn"
+         disabled={label.length == 0}
+       >
+         Save
+       </Button>
+     </div>
+   </div>
+ );
 }
-
-export default Label
+ 
+export default Label;
+ 
